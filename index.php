bff27de67e778f0138f68c00cc5dc81203e32c08
Nathaelle <scriptales.clients@gmail.com>
2020-12-04 14:54:14 +0100

    Correction syntaxique

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
 
 require_once "Transmettre.php";
-require_once "ActeMedical";
-require_once "ActeConventionneTraitant";
-require_once "ActeConventionneNonTraitant";
-require_once "ActeNonConventionne";
+require_once "ActeMedical.php";
+require_once "ActeConventionneTraitant.php";
+require_once "ActeConventionneNonTraitant.php";
+require_once "ActeNonConventionne.php";
+
+$acte1 = new ActeConventionneTraitant("Dupond", 156325698554, "MDC1568");
+$acte2 = new ActeConventionneNonTraitant("Durand", 21569845697, "456987");
+$acte3 = new ActeNonConventionne("Michel", 16985632566, "45SQD698632", 40);
+
